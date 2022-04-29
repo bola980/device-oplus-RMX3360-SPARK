39d3749e6c75f98a7e6ae12c18dccf77a0db6c86
pjgowtham <pjgowtham@gmail.com>
2022-04-29 19:31:17 +0530

    RMX3360: Add logic for prebuilt kernel/DTB/DTBO

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,20 @@ DEVICE_PATH := device/oplus/RMX3360
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# Boot/DTBO
+DEVICE_USES_PREBUILT_DTB := true
+PREBUILT_PATH := $(DEVICE_PATH)/prebuilt
+
+ifeq ($(DEVICE_USES_PREBUILT_DTB),true)
+BOARD_PREBUILT_DTBOIMAGE := $(PREBUILT_PATH)/dtbo.img
+TARGET_PREBUILT_DTB := $(PREBUILT_PATH)/dtb.img
+endif
+
+ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
+DEVICE_USES_PREBUILT_DTB := true
+TARGET_PREBUILT_KERNEL := $(PREBUILT_PATH)/Image.gz
+endif
+
 # Common
 include device/oplus/sm8350-common/BoardConfigCommon.mk
 
